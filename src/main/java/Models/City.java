84687cb93c43e53f5b9ee1f58156cbd9834e7e18
Verkin78 <otrajenie.jc@gmail.com>
2023-02-13 18:13:23 +0300

    Create City

---
+++ src/main/java/Models/City.java
@@ -0,0 +1,17 @@
+package Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+@NonNull
+public class City {
+    private  int id;
+    private final String cityName;
+
+    public City(String cityName) {
+        this.cityName = cityName;
+    }
+}
