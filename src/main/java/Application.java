57a8233f7db103168959997c6b322b51780817a0
Verkin78 <otrajenie.jc@gmail.com>
2023-02-15 18:22:59 +0300

    Change Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,54 +1,24 @@
 import models.Employee;
 import services.EmployeeDAO;
 import services.EmployeeDAOImpl;
-import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
 
-    public static void main(String[] args) throws SQLException {
-        final String user = "postgres";
-        final String password = "DthjybrfZ";
-        final String url = "jdbc:postgresql://localhost:5432/skypro";
-        Connection connection = DriverManager.getConnection(url, user, password);
-        EmployeeDAO employeeDAO = new EmployeeDAOImpl(connection);
-
-        Employee employee = new Employee("Sergey",
-                "Solovyev",
+    public static void main(String[] args)  {
+        EmployeeDAO employeeDAO = new EmployeeDAOImpl();
+        Employee employee = new Employee("Andrey",
+                "Davidov",
                 "man",
-                26,2);
-       // employeeDAO.getAllEmployees();
-       // testDataBase(connection);
+                9,
+                19,
+                5);
+        // employeeDAO.createEmployee(employee);
+        // System.out.println(employeeDAO.getEmployeeById(1));
+        //  employeeDAO.setEmployeeCityByid(employee);
+        // employeeDAO.deleteEmployeeById(employee);
         List<Employee> employeeList = employeeDAO.getAllEmployees();
         employeeList.forEach(System.out::println);
-    }
-
-    public static void testDataBase(Connection connection) {
-            try {
-            PreparedStatement preparedStatement = connection
-                    .prepareStatement("SELECT first_name," +
-                            "last_name," +
-                            "gender," +
-                            "age," +
-                            "city_name" +
-                            " FROM employee " +
-                            "INNER JOIN city ON employee.city_id = city.city_id WHERE id =(?)");
-            preparedStatement.setInt(1,4);
-            final ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                String firstName = "firstname: "+resultSet.getString("first_name");
-                String lastName = "lastname: "+resultSet.getString("last_name");
-                String gender = "gender: "+resultSet.getString("gender");
-                int age = resultSet.getInt("age");
-                String cityName = "City: " + resultSet.getString("city_name");
-                System.out.println(firstName);
-                System.out.println(lastName);
-                System.out.println(gender);
-                System.out.println("Age: "+age);
-                System.out.println(cityName);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
