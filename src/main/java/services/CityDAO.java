406a9d404cb008f1372011ecdd23144eca6e4ac1
Verkin78 <otrajenie.jc@gmail.com>
2023-02-17 14:46:36 +0300

    Change for CityDAO

--- src/main/java/services/CityDAO.java
+++ src/main/java/services/CityDAO.java
@@ -1,16 +1,13 @@
-package models;
+package services;
 
 import models.City;
 import models.Employee;
-
 import java.util.List;
 
 public interface CityDAO {
-
     void createCity(City city);
 
     City getCityById(long id);
 
     List<City> getAllCity();
-
 }
