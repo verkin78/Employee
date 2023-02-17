5cebafb948c54b55a704428ddfa284be9e16c987
Verkin78 <otrajenie.jc@gmail.com>
2023-02-17 14:59:58 +0300

    Change for Appli

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,7 @@
+import models.City;
 import models.Employee;
+import services.CityDAO;
+import services.CityDAOImpl;
 import services.EmployeeDAO;
 import services.EmployeeDAOImpl;
 import java.util.ArrayList;
@@ -6,18 +9,27 @@ import java.util.List;
 
 public class Application {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         EmployeeDAO employeeDAO = new EmployeeDAOImpl();
-        Employee employee = new Employee("Andrey",
-                "Davidov",
+        Employee employee = new Employee("Vladimir",
+                "Dmitriev",
                 "man",
-                9,
-                5);
-        // employeeDAO.createEmployee(employee);
+                4,
+                26,
+                new City(1));
+        //employeeDAO.createEmployee(employee);
         // System.out.println(employeeDAO.getEmployeeById(1));
-        //  employeeDAO.setEmployeeCityByid(employee);
-        // employeeDAO.deleteEmployeeById(employee);
-       List<Employee> employeeList = employeeDAO.getAllEmployees();
+        // employeeDAO.setEmployeeCityByid(employee);
+        //   employeeDAO.deleteEmployeeById(35);
+        List<Employee> employeeList = employeeDAO.getAllEmployees();
         employeeList.forEach(System.out::println);
+        //testHibernateTheTableCity();
+    }
+
+    public static void testHibernateTheTableCity() {
+        CityDAO cityDao = new CityDAOImpl();
+        // cityDao.createCity(new City("Norilsk"));
+        List<City> cityList = cityDao.getAllCity();
+        cityList.forEach(System.out::println);
     }
 }
