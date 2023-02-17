39714361a7f99db9e30aa8cc9116375c1a98c233
Verkin78 <otrajenie.jc@gmail.com>
2023-02-17 14:35:35 +0300

    Change for City

--- src/main/java/models/City.java
+++ src/main/java/models/City.java
@@ -1,27 +1,39 @@
 package models;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-
 import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@NonNull
 @Entity
 @Table(name = "city")
 public class City {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy  = GenerationType.IDENTITY)
     @Column(name = "city_id")
     private  long id;
     @Column(name = "city_name")
     private  String cityName;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "city")
+    private List<Employee> employees;
+
+    public City(long id) {
+        this.id = id;
+    }
 
     public City(String cityName) {
-        this.cityName = cityName;
+        if (Objects.isNull(cityName)) {
+            this.cityName = "No City";
+        } else {
+            this.cityName = cityName;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return cityName;
     }
 }
